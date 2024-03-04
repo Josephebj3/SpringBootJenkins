eac9f9d63ced10bcdf538daec265a07dbac72d1d
JosephJohnson <JoeTestJohn@gmail.com>
2024-03-04 16:24:40 -0600

    first push for project

---
+++ src/main/java/com/cogent/main/SpringBootJenkinsApplication.java
@@ -0,0 +1,62 @@
+package com.cogent.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication 
+{
+	static Logger log = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	
+	@PostConstruct
+	void init() 
+	{
+		log.debug("Its init post construct statement");
+	}
+	
+	public static void main(String[] args) {
+		log.error("found error in first line of main");
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
